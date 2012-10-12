cf7d82a8de8da63c3f580d3265a96d0c1c75f621
root <root@mage5.nexcess.net>
2012-10-12 12:17:46 -0400

    adding CLI wrapper

---
+++ rediscli.php
@@ -0,0 +1,29 @@
+<?php
+include_once("credis/Client.php");
+
+abstract class Zend_Cache
+{
+    const CLEANING_MODE_ALL              = 'all';
+    const CLEANING_MODE_OLD              = 'old';
+    const CLEANING_MODE_MATCHING_TAG     = 'matchingTag';
+    const CLEANING_MODE_NOT_MATCHING_TAG = 'notMatchingTag';
+    const CLEANING_MODE_MATCHING_ANY_TAG = 'matchingAnyTag';
+    function throwException($text) {
+	die($text."\n");
+    }
+}
+
+abstract class Zend_Cache_Backend
+{
+}
+
+interface Zend_Cache_Backend_ExtendedInterface 
+{
+}
+
+include_once("Redis.php");
+
+$cache = new Cm_Cache_Backend_Redis(array('server' => "127.0.0.1", 'port' => "6379", 'database' => 1));
+$cache->clean(Zend_Cache::CLEANING_MODE_OLD);
+
+?>
